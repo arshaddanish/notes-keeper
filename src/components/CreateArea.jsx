4a6407eb5980556503ad41def817df50fcdc997d
Arshad Danish <arshaddanishthana@gmail.com>
2021-03-25 14:13:19 +0530

    Add expanding functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -4,6 +4,8 @@ import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [init, setInit] = React.useState(false);
+
   const [note, setNote] = React.useState({
     title: "",
     content: "",
@@ -23,28 +25,38 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function initHandle() {
+    setInit(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          onChange={changeHandle}
-          name="title"
-          placeholder="Title"
-          autoComplete="off"
-          value={note.title}
-        />
-        <textarea
-          onChange={changeHandle}
-          name="content"
-          placeholder="Take a note..."
-          rows="3"
-          value={note.content}
-        />
-        <Zoom in={true}>
-          <Fab onClick={clickHandle}>
-            <AddIcon />
-          </Fab>
-        </Zoom>
+        {init ? (
+          <div>
+            <input
+              onChange={changeHandle}
+              name="title"
+              placeholder="Title"
+              autoComplete="off"
+              value={note.title}
+            />
+            <textarea
+              onChange={changeHandle}
+              name="content"
+              placeholder="Take a note..."
+              rows="3"
+              value={note.content}
+            />
+            <Zoom in={true}>
+              <Fab onClick={clickHandle}>
+                <AddIcon />
+              </Fab>
+            </Zoom>
+          </div>
+        ) : (
+          <input onClick={initHandle} placeholder="Take a note..."/>
+        )}
       </form>
     </div>
   );
